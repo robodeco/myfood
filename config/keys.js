3346ef8626b8c2b1b64fb88a82fe930205799cc6
Alex H <08ahayden@gmail.com>
2018-03-24 10:28:49 -0400

    troubleshooting heroku

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//add this file to .gitignore
+
+module.exports = {
+google: {
+  clientID:'210638088912-bch7pvvsfpjb184jl04df6cdfje67gur.apps.googleusercontent.com',
+  clientSecret:'VV2rpJcl7gtDFNnBkcj9c5pR'
+}
+
+};
