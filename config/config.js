a160d37ea7a7976faeb643b6e6eebd8e36966ea5
Alex H <08ahayden@gmail.com>
2018-03-24 10:05:41 -0400

    jawsdb

--- config/config.js
+++ config/config.js
@@ -15,10 +15,12 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "use_env_variable": "mysql://ui038tjb8kdbwph1:u9lbzj3rbi7tixci@h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jkj08cavanseit78",
+    "dialect": "mysql"
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": "jkj08jkj08cavanseit78",
+    "host": "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 
